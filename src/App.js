cad27ebff6fc182d9f020dd2a2b9b645a74239c6
Zhangbokai614 <614525683@qq.com>
2022-12-02 18:37:41 +0800

    update: add theme manage and init app page layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Layout, Menu, Image, ConfigProvider } from 'antd';
 
-function App() {
+import { routers, menuItems } from './routers';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [currentPage, setCurrentPage] = useState('Home')
+
+  const switchPage = (value) => {
+    setCurrentPage(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00b96b',
+        },
+      }}
+    >
+      <Layout
+        style={{
+          minHeight: '100vh',
+        }}
+      >
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+          <Image
+            width={50}
+            src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+          />
+          <Menu theme="dark" defaultSelectedKeys={['Home']} mode="vertical" items={menuItems} onClick={(item) => { switchPage(item.key) }} />
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+            className="site-layout-background"
+            style={{
+              padding: 0,
+            }}
+          />
+          <Content
+            style={{
+              margin: '0 16px',
+            }}
+          >
+            {routers[currentPage]}
+          </Content>
+          <Footer
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
   );
-}
-
+};
 export default App;
