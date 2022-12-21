9bf89f424311a5c8490f6f132eb828741f9c1e3b
ZEQUANR <675897584@qq.com>
2022-12-21 20:19:45 +0800

    update: revise article page

--- src/pages/article/index.jsx
+++ src/pages/article/index.jsx
@@ -1,13 +1,26 @@
-import React from "react"
-import "antd/dist/reset.css"
-import Article from "./components/Article"
-import Cover from "./components/Cover"
+import React, { useState, useEffect } from "react"
+import services from "../../services/index"
+import Cover from "../../components/text-markdown"
+import "./index.css"
 
-const ArticlePage = () => (
-  <div className="App">
-    <Article></Article>
-    <Cover></Cover>
-  </div>
-)
+const ArticlePage = () => {
+  const { currentUser } = services.article
+  const [data, setData] = useState({ hits: "" })
+  useEffect(() => {
+    const requestData = async () => {
+      const { data } = await currentUser()
+      setData(data)
+    }
+    requestData()
+  }, [currentUser])
+  return (
+    <>
+      <div className="Article-Container">
+        <div className="Article-Container-middle"></div>
+      </div>
+      <Cover title={"文章标题"} author={"作者： kkk"} text={data}></Cover>
+    </>
+  )
+}
 
 export default ArticlePage
