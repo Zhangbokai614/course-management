4616e78b2ba9ef8ec1cbafba17624c9543cfbac2
Zhangbokai614 <614525683@qq.com>
2022-12-22 09:08:48 +0800

    update: style switch to ConfigProvider token

--- src/pages/codeing/index.jsx
+++ src/pages/codeing/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import MonacoEditor from 'react-monaco-editor/lib/editor';
-import { Select } from 'antd';
+import { Select, theme } from 'antd';
 import { ReloadOutlined } from '@ant-design/icons';
 import 'antd/dist/reset.css';
 
@@ -9,13 +9,18 @@ import compare from './compare';
 import './index.css'
 
 const defaultLanguages = languages[0].value
+const { useToken } = theme;
+
 
 const CodeingPage = () => {
+  const { token } = useToken();
+
   const [language, setLanguage] = useState(defaultLanguages);
   const [currentCompares, setCurrentCompares] = useState(compare[language]);
 
   const selectHandleChange = (value) => {
     setLanguage(value)
+
     setCurrentCompares(compare[value])
   }
 
@@ -27,7 +32,7 @@ const CodeingPage = () => {
     console.log(editor)
   };
 
-  const options = {
+  const monacoOptions = {
     selectOnLineNumbers: true,
     roundedSelection: true,
     readOnly: false,
@@ -37,9 +42,22 @@ const CodeingPage = () => {
     folding: true,
   };
 
+  const boxStyle = {
+    borderRadius: token.borderRadius,
+    backgroundColor: token.colorBgContainer,
+    color: token.colorTextSecondary,
+  }
+
   return (
-    <div className='codeing-root'>
-      <div className='editor'>
+    <div
+      className='codeing-root'
+      style={{
+        borderRadius: token.borderRadius,
+        backgroundColor: token.colorBgLayout,
+        color: token.colorTextSecondary,
+      }}
+    >
+      <div className='editor' style={{ borderRadius: token.borderRadius }}>
         <div className='header'>
           <p className='text' >Editor</p>
           <Select
@@ -49,13 +67,13 @@ const CodeingPage = () => {
             options={languages}
           />
         </div>
-        <div className='editor-body'>
+        <div className='editor-body' style={boxStyle}>
           <MonacoEditor
             className={'monaco-editor'}
             height="80vh"
             language={language}
             value=""
-            options={options}
+            options={monacoOptions}
             onChange={onChange}
             editorDidMount={(editor) => editorDidMount(editor)}
             theme="vs-light"
@@ -66,20 +84,22 @@ const CodeingPage = () => {
         <div className='header'>
           <p className='text'>Execution stdin</p>
         </div>
-        <div className='stdin'></div>
+        <div className='stdin' style={boxStyle}></div>
         <div className='compler'>
           <p className='text'>Compler</p>
           <Select
-            defaultValue={currentCompares[0]}
+            key={currentCompares[0].value}
+            defaultValue={currentCompares[0].label}
             style={{ width: "10vw" }}
             options={currentCompares}
+            showSearch={true}
           />
           <p className='text'>State</p>
-          <ReloadOutlined/>
+          <ReloadOutlined />
         </div>
-        <div className='stdout'></div>
+        <div className='stdout' style={boxStyle}></div>
         <p className='text'>Info</p>
-        <div className='info'></div>
+        <div className='info' style={boxStyle}></div>
       </div>
     </div>
   );
