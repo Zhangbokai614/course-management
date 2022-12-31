837bcbe7b2a9b0655a201b360ee88cdd4c391650
ZEQUANR <675897584@qq.com>
2022-12-31 16:08:45 +0800

    update: article and course list request code

--- src/services/article.js
+++ src/services/article.js
@@ -3,7 +3,7 @@ import request from "umi-request"
 export async function currentUser(options) {
   return request("/article/markdown", {
     method: "GET",
-    prefix: "http://192.168.0.24:4000",
+    prefix: "http://localhost:4000",
     ...(options || {}),
   })
 }
--- src/services/course-list.js
+++ src/services/course-list.js
@@ -1,9 +1,17 @@
 import request from "umi-request"
 
-/** 获取当前的用户 GET /api/currentUser */
-export async function User(options) {
-  return request("192.168.0.24:8081/article", {
+export async function tabControl(options) {
+  return request("/courseList/", {
     method: "GET",
+    prefix: "http://localhost:4000",
+    ...(options || {}),
+  })
+}
+
+export async function User(url, options) {
+  return request("/courseList/" + url, {
+    method: "GET",
+    prefix: "http://localhost:4000",
     ...(options || {}),
   })
 }
